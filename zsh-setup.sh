4260e994a7b8ae45418380601e623de40dcdc0a5
David Conner <dconner.pro@gmail.com>
2012-11-11 11:40:43 -0500

    added link-zshrc method

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -7,11 +7,16 @@ dcdotfiles-vars-setzsh()
 
 dcdotfiles-link-omz()
 {
-  echo 'TODO: link omz'
+  dcdotfiles-make-symlink $INSTALL_PATH/omz $HOME_PATH/.oh-my-zsh
 }
 
 dcdotfiles-link-zshrc()
 {
-  echo 'TODO: link zshrc'
+  [[ ! -d "$HOME_PATH/.zsh" ]] && mkdir $HOME_PATH/.zsh
+  dcdotfiles-make-symlink $INSTALL_PATH/zsh/.zshrc.$OS_TYPE $HOME_PATH/.zshrc
+  dcdotfiles-make-symlink $INSTALL_PATH/zsh/zsh/.aliases.$OS_TYPE $HOME_PATH/.zsh/.aliases
+  dcdotfiles-make-symlink $INSTALL_PATH/zsh/zsh/.bindings.$OS_TYPE $HOME_PATH/.zsh/.bindings
+  dcdotfiles-make-symlink $INSTALL_PATH/zsh/zsh/.omz $HOME_PATH/.zsh/.omz
 }
+
 
