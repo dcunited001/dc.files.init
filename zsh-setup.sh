827fe9e038b450f94e631c5334d9c62db5d00e14
David Conner <dconner.pro@gmail.com>
2012-11-11 10:53:34 -0500

    stubbed out zsh methods

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -2,6 +2,16 @@
 
 dcdotfiles-vars-setzsh()
 {
+  echo 'TODO: set zsh vars'  
+}
 
+dcdotfiles-link-omz()
+{
+  echo 'TODO: link omz'
+}
+
+dcdotfiles-link-zshrc()
+{
+  echo 'TODO: link zshrc'
 }
 
