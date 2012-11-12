ee2332e86b791ca0e3a81518d795121545e0db61
David Conner <dconner.pro@gmail.com>
2012-11-12 11:37:17 -0500

    added defaults to mac script temporarily

--- mac.sh
+++ mac.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 source init/support.sh
-vars-setinit
+# vars-setinit
+export OS_TYPE=mac
+export INSTALL_NAME='David Conner'
+export INSTALL_EMAIL='dconner.pro@gmail.com'
+export INSTALL_USER_NAME=davidconner
+export INSTALL_GIT_USER=dcunited001
+export INSTALL_HOME_PATH=/Users/davidconner
+export INSTALL_FOLDER=.files
+export INSTAPP_PATH=$INSTALL_HOME_PATH/$INSTALL_FOLDER
+
 check-dependencies-init
+
