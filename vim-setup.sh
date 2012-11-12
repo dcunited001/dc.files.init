6d09eac086eb5ee4d129e1e7a81754a7582baae3
David Conner <dconner.pro@gmail.com>
2012-11-12 05:49:25 -0500

    added linking for vimrc and gvimrc

--- vim-setup.sh
+++ vim-setup.sh
@@ -2,7 +2,7 @@
 
 check-dependencies-vim()
 {
-  echo 'TODO: check-dependencies-vim'
+  check-if-installed vim
 }
 
 vars-setvim()
@@ -10,3 +10,19 @@ vars-setvim()
   echo 'TODO: set vim vars'
 }
 
+setup-vim-plugins()
+{
+  echo 'TODO: setup-vim-plugins'
+}
+
+link-vimrc()
+{
+  make-symlink $INSTALL_PATH/vim/.vimrc.before.$OS_TYPE $HOME_PATH/.vimrc.before
+  make-symlink $INSTALL_PATH/vim/.vimrc.after.$OS_TYPE $HOME_PATH/.vimrc.after  
+}
+
+link-gvimrc()
+{
+  make-symlink $INSTALL_PATH/vim/.gvim.before.$OS_TYPE $HOME_PATH/.gvim.before
+  make-symlink $INSTALL_PATH/vim/.gvim.after.$OS_TYPE $HOME_PATH/.gvim.after
+}
