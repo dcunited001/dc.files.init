cae43493e4de6f297d95f5e2fc4e651ec1e13a6b
David Conner <dconner.pro@gmail.com>
2012-11-12 11:39:09 -0500

    reformatted

--- vim-setup.sh
+++ vim-setup.sh
@@ -4,16 +4,9 @@ check-dependencies-vim(){ check-if-installed vim }
 vars-setvim(){ echo 'TODO: set vim vars' }
 setup-vim-plugins(){ echo 'TODO: setup-vim-plugins' }
 
-link-vimrc(){
-  make-symlink $INSTALL_PATH/vim/.vimrc.before.$OS_TYPE $HOME_PATH/.vimrc.before
-  make-symlink $INSTALL_PATH/vim/.vimrc.after.$OS_TYPE $HOME_PATH/.vimrc.after  
-}
+link-vimrc-before()   { make-symlink $INSTALL_PATH/vim/.vimrc.before.$OS_TYPE $HOME_PATH/.vimrc.before }
+link-vimrc-after()    { make-symlink $INSTALL_PATH/vim/.vimrc.after.$OS_TYPE $HOME_PATH/.vimrc.after }
+link-gvimrc-before()  { make-symlink $INSTALL_PATH/vim/.gvim.before.$OS_TYPE $HOME_PATH/.gvim.before } 
+link-gvimrc-after()   { make-symlink $INSTALL_PATH/vim/.gvim.after.$OS_TYPE $HOME_PATH/.gvim.after }
 
-link-gvimrc(){
-  make-symlink $INSTALL_PATH/vim/.gvim.before.$OS_TYPE $HOME_PATH/.gvim.before
-  make-symlink $INSTALL_PATH/vim/.gvim.after.$OS_TYPE $HOME_PATH/.gvim.after
-}
-
-link-vim-colors(){
-  make-symlink $INSTALL_PATH/vim/colors/ $HOME_PATH/.vim/colors
-}
+link-vim-colors()     { make-symlink $INSTALL_PATH/vim/colors/ $HOME_PATH/.vim/colors }
