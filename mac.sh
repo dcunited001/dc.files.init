06298c9a67f800bd81b3d0aa4274bfd537c4f0a4
David Conner <dconner.pro@gmail.com>
2012-11-12 22:50:50 -0500

    set up to go to main menu

--- mac.sh
+++ mac.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 source init/support.sh
 
-# vars-setinit
-vars-setinit-default-mac
+vars-setinit-defaults-mac #sets default values
+vars-setinit-dc-mac #automates, for now
 
 output-info
 check-dependencies-init
 
+menu-main
