790a82fd8e9d4bd30b54527c223efb5d48df6791
David Conner <dconner.pro@gmail.com>
2012-11-12 19:35:46 -0500

    refactored to use input helpers

--- subl-setup.sh
+++ subl-setup.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-check-dependencies-subl(){ echo 'TODO: check-dependencies-subl' }
+check-dependencies-subl(){ output-todo 'check-dependencies-subl' }
+
+DEFAULT_SUBL_SETTINGS_PATH_MAC='/Library/Application Support/Sublime Text 2/Packages/User'
+DEFAULT_SUBL_SETTINGS_USERFILE_MAC='Preferences.sublime-settings'
+DEFAULT_SUBL_SETTINGS_PATH_UBU=
+DEFAULT_SUBL_SETTINGS_USERFILE_UBU=
 
 vars-setsubl(){
+  output-todo 'sublime plugins/settings' # export SUBL_PLUGINS_PATH=$2
   case "$OS_TYPE" in
-    mac)
-      export SUBL_SETTINGS_PATH="$HOME_PATH/Library/Application Support/Sublime Text 2/Packages/User"
-      export SUBL_SETTINGS_USER='Preferences.sublime-settings'
-      ;;
-    ubu)
-      # TODO: Linux
-      export SUBL_SETTINGS_PATH=
-      export SUBL_SETTINGS_USER=
-      ;;
-  esac
-  # TODO: sublime plugins/settings
-  #export SUBL_PLUGINS_PATH=$2
-}
+    mac) vars-setsubl-path $DEFAULT_SUBL_SETTINGS_PATH_MAC $DEFAULT_SUBL_SETTINGS_USERFILE_MAC;;
+    ubu) vars-setsubl-ubu $DEFAULT_SUBL_SETTINGS_PATH_UBU $DEFAULT_SUBL_SETTINGS_USERFILE_UBU;;
+  esac; }
 
+vars-setsubl-path(){
+  resolve-and-set 'SUBL_SETTINGS_PATH', $1
+  resolve-and-set 'SUBL_SETTINGS_USERFILE', $1 }
+  
 link-subl(){ make-symlink $INSTALL_PATH/subl/$SUBL_SETTINGS_FILE $SUBL_SETTINGS_PATH/$SUBL_SETTINGS_FILE }
-backup-subl(){ echo 'TODO: backup subl settings' }
+backup-subl(){ output-todo 'backup subl settings' }
 
