333d3de52c0620886a6e45a6b6eee25dbea59a0f
David Conner <dconner.pro@gmail.com>
2012-11-14 12:48:47 -0500

    updated with new paths

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -19,5 +19,5 @@ link-omz(){ make-symlink $INSTALL_PATH/omz $INSTALL_HOME_PATH/.oh-my-zsh; }
 link-zshrc(){
   mkdir-if-missing $INSTALL_HOME_PATH/.zsh
   make-symlink $INSTALL_PATH/zsh/.zshrc.$OS_TYPE $INSTALL_HOME_PATH/.zshrc
-  make-symlink $INSTALL_PATH/zsh/zsh/.aliases.$OS_TYPE $INSTALL_HOME_PATH/.zsh/.aliases
-  make-symlink $INSTALL_PATH/zsh/zsh/.omz $INSTALL_HOME_PATH/.zsh/.omz; }
+  make-symlink $INSTALL_PATH/zsh/.aliases.$OS_TYPE $INSTALL_HOME_PATH/.zsh/.aliases
+  make-symlink $INSTALL_PATH/zsh/.omz $INSTALL_HOME_PATH/.zsh/.omz; }
