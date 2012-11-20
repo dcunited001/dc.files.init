bf4a5cbb8da1db49e4beae126e15575b58f4fc3f
David Conner <dconner.pro@gmail.com>
2012-11-19 20:41:28 -0500

    added function to backup plist

--- iterm-setup.sh
+++ iterm-setup.sh
@@ -8,16 +8,17 @@ setup-iterm(){
   opts=(
     'check-dependencies-iterm'
     'vars-setiterm'
-    'link-iterm'
-    # 'backup-iterm'
     'menu-setup'
     'menu-exit' );
   menu-for ${opts[@]};
-  setup-iterm; }  
+  setup-iterm; }
 
 vars-setiterm(){
   ask-for-input 'ITERM_SETTINGS_PATH'; 
   ask-for-input 'ITERM_SETTINGS_FILE'; }
-link-iterm(){ make-symlink $INSTALL_PATH/iterm/$ITERM_SETTINGS_FILE $ITERM_SETTINGS_PATH/$ITERM_SETTINGS_FILE; }
-backup-iterm(){ output-todo 'backup iterm settings'; }
 setup-iterm-fonts(){ output-todo 'setup iterm fonts'; }
+backup-iterm(){
+  newpath=$INSTALL_PATH/iterm/$ITERM_SETTINGS_FILE
+  cp $ITERM_SETTINGS_PATH/$ITERM_SETTINGS_FILE $newpath
+  plutil -convert xml1 -o $newpath.xml; }
+
