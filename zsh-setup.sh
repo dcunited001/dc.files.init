24dc36e3c45ecc8613b14dc228fcc1314d3ae938
David Conner <dconner.pro@gmail.com>
2012-11-19 20:33:14 -0500

    changed to refer to non-hidden files

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -18,6 +18,13 @@ vars-setzsh(){ output-todo 'set zsh vars'; }
 link-omz(){ make-symlink $INSTALL_PATH/omz $INSTALL_HOME_PATH/.oh-my-zsh; }
 link-zshrc(){
   mkdir-if-missing $INSTALL_HOME_PATH/.zsh
-  make-symlink $INSTALL_PATH/zsh/.zshrc.$OS_TYPE $INSTALL_HOME_PATH/.zshrc
-  make-symlink $INSTALL_PATH/zsh/.aliases.$OS_TYPE $INSTALL_HOME_PATH/.zsh/.aliases
-  make-symlink $INSTALL_PATH/zsh/.omz $INSTALL_HOME_PATH/.zsh/.omz; }
+  make-symlink $INSTALL_PATH/zsh/zshenv.$OS_TYPE $INSTALL_HOME_PATH/.zsh/.zshenv
+  make-symlink $INSTALL_PATH/zsh/zprofile.$OS_TYPE $INSTALL_HOME_PATH/.zsh/.zprofile
+  make-symlink $INSTALL_PATH/zsh/zshrc.$OS_TYPE $INSTALL_HOME_PATH/.zsh/.zshrc
+  make-symlink $INSTALL_PATH/zsh/zlogin.$OS_TYPE $INSTALL_HOME_PATH/.zsh/.zlogin
+  make-symlink $INSTALL_PATH/zsh/zlogout.$OS_TYPE $INSTALL_HOME_PATH/.zsh/.zlogout
+  make-symlink $INSTALL_PATH/alias/alias.$OS_TYPE $INSTALL_HOME_PATH/.zsh/.alias
+  make-symlink $INSTALL_PATH/zsh/omz $INSTALL_HOME_PATH/.zsh/.omz; }
+
+link-zsh-profile-etc(){ output todo 'create /etc/zsh* links (or copy the files?)'; }
+backup-zsh-profile-etc() { output todo 'cp /etc/zsh* files'; }
